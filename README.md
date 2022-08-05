6bf01b68ffdb247dc30d1b98f077d2464cb36ea2
BaertA <arsene.baert@etu.emse.fr>
2022-08-05 16:55:23 -0400

    Bug fix, add Flow Chart, complete README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-**Communication entre python et LabView:**
+# Communication TCP/IP entre python et LabView:
 
-Dans le cadre de notre projet, nous utilisons un des pédales sensix contenant des capteurs. Les données récoltés par les capteurs sont envoyés à une boîte d'acquisition de la marque National-Instrument pour traitement. La carte NI (National-Instrument) est programmée en LabView, les données sont donc disponible sur cette interface de programmation. Pour récupérer les données, il est donc nécessaire de faire communiquer LabView avec python. Il a été choisi d'utiliser le protocol TCP/IP pour cette communication, car celui-ci est très versatile, peut s'interfacer avec de nombreux langages et est simple d'utilisation.
+Dans le cadre de notre projet, nous utilisons un des pédales sensix contenant des capteurs. Les données récoltés par les capteurs sont envoyées à une boîte d'acquisition de la marque National-Instrument pour traitement. La carte NI (National-Instrument) est programmée en LabView, les données sont donc disponible sur cette interface de programmation. Pour récupérer les données, il est donc nécessaire de faire communiquer LabView avec python. Il a été choisi d'utiliser le protocol TCP/IP pour cette communication, car celui-ci est très versatile, peut s'interfacer avec de nombreux langages et est simple d'utilisation.
 
 
 
-**TCP/IP en python - Le client:**
+## TCP/IP python - Le client:
 
 Le code python se base sur un code préexistant développé pour le logicielle biosiglive, il a été modifié pour s'adapter au mieux à notre problématique.
 
@@ -12,19 +12,9 @@ Par exemple, le transtypage des données vers des "bytes" (octet) se fait avec l
 
 Autre exemple, dans le cas de biosiglive, on ouvre la connexion et on la ferme à chaque itération. Tandis que dans notre cas, pour des raisons d'optimisation, on ouvre le port une  seule fois au début.
 
-**TCPC/IP Labview - Le server:**
 
 
-
-**Structure des données sur Labview:**
-
-On se base sur une matrice de 15 par 10 (15 lignes, 10 colonnes) contenant des floats.
-
-1. Conversion de la matrice en données envoyables sur le TCP/IP
-
-Le client envoie le type de donnée qu'il désire, 
-
-Pour l'exemple on va considérer une attribution arbitraire des données :
+Le client contient la table de correspondance entre le nom des données et leur coordonnées dans la matrice. Pour l'exemple, on suppose que les données sont structurées comme suit dans une matrice de 15 lignes, 10 colonnes:
 
 - Les forces gauches et droites sont sur la première ligne, chaque force est décomposée en 3 valeurs selon les axes x, y et z (6 valeurs). La nomenclature est FGx pour force gauche sur l'axe x. Ordonnées de droite à gauche avec premièrement les forces relatives au côté gauche puis droit, et en suivant l'ordre x, y et z.
 
@@ -34,39 +24,53 @@ Pour l'exemple on va considérer une attribution arbitraire des données :
 
 - les angles sont sur la 7ème ligne, il y a les angles gauche et droit.
 
-- Le reste des données ne sont pour l'instant pas utiliser.
-
-Le serveur reçoit une liste de toutes les coordonnées des données voulues. Et retourne une sur les 4 premiers octets la taille des données qu'il renvoie puis les données ordonnées dans l'ordre de la liste reçue.
-
-Étant donnée que les données échanger sur TCP/IP dans labview doivent être de type string on choisit de faire un transtypage permettant de garder les données sous leur formes binaires, cela nous permet de garder une taille toujours identique d'une donnée quelle que soit la donnée. (exemple: si l'on veut envoyer un float 32, on envoie la valeur binaire du float 32 et non pas une représentation en ASCII). source : https://knowledge.ni.com/KnowledgeArticleDetails?id=kA00Z00000159spSAA&l=en-CA
-
-Il ne faut pas oublier que la matrice est renouvelée à une fréquence d'environ 30Hz. On va donc vérifier que l'on ne doit pas attendre de recevoir qq chose de la part du client pour renouveler la valeur de la matrice.
+- Le reste des données ne sont pour l'instant pas utilisées dans notre exemple.
 
 
 
-**Protocole utilisé:**
+## TCP/IP Labview - Le server:
 
-Le protocole utilisé est bien évidement TCP/IP. On va détailler ici quelles sont les données échangée et leur rôle.
+### Structure des données sur Labview:
 
-![image-20220804181857459](C:\Users\arsen\AppData\Roaming\Typora\typora-user-images\image-20220804181857459.png) 
+La donnée se présente sous forme d'une matrice 15 lignes 10 colonnes de floats 32 ("data array.vi"). Elle est pour l'instant générée aléatoirement après un temps choisit dans "data array refresh.vi". 
+
+Cette création de données se faisant dans une boucle while indépendante de la boucle while du programme principal, on doit créer une queue permettant aux deux process de communiquer. Celle-ci ne contient au maximum qu'un seul élément, lors de la génération d'une nouvelle matrice, on vide la queue s'il est pleine pour venir écrire la nouvelle donnée.
+
+## Utilisation de la version non intégrée au programme:
+
+1. Ouvrir le fichier client.py et choisir le mode d’exécution.
+2. Ouvrir le fichier  Server_LabView.vi.
+3. Si l'on veut modifier la fréquence de génération de donnée, cela peut se faire en modifiant le paramètre "Generation period (ms)"
+4. Lancer l'exécution de Server_LabView.vi.
+5. Lancer l'exécution de client.py.
+
+## Intégration au programme existant "I-Crankset":
+
+Pour intégrer notre module TCP/IP au programme existant, il est nécessaire de:
+
+- Donner la matrice (ou sont type, c'est à dire une matrice de taille équivalente) des données générées à l'entrée de la création de la queue dans le VI "Server_LabView.vi", ce qui permet à la queue d'avoir le bon type de donnée qu'elle va stocker.
+
+  ![image-20220805120329358](C:\Users\arsen\AppData\Roaming\Typora\typora-user-images\image-20220805120329358.png)
+
+- Récupérer le "queue out", contenant le queue refnum et le type des données échangé avec la queue , et le "error in". Cette connexion entre le VI du programme de récolte de données et notre programme se fait dans le VI "Server_LabView.vi" en remplaçant le vi "data array refresh.vi" par le VI du programme.
+
+  ![image-20220805120346555](C:\Users\arsen\AppData\Roaming\Typora\typora-user-images\image-20220805120346555.png)
+
+- Une fois les données générées il faut, effacer ce qu'il y a dans la queue (s'il y a qq chose) avec le module dequeue element avec le time out à 0. Puis mettre les données générées dans la queue avec le module Enqueue element (sans time out). Comme dans le fichier "data array refresh.vi".
+
+
+
+## Protocole utilisé:
+
+Le protocole utilisé est bien évidement TCP/IP. On va détailler ici quelles sont les données échangée et leur rôle. En annexe se trouve une image contenant un diagramme "Diagramme communication" montrant le fonctionnement de l'échange TCP/IP entre python et LabView.
 
 Le client commence par envoyé une trame construite comme suit :
 
 1. Les 4 premiers octets représente la taille de la trame (sans compter ces 4 octets) sous forme d'un signed int 32.
-2. La deuxième partie de la trame est composée des coordonnées matricielle des données que l'on veut, chaque coordonné est stocké sur un unsigned char pour prendre le moins de place possible, la coordonnées maximum vaut 15 et est donc inférieur à la valeur maximum pouvant être stockée sur un char (255).
+2. La deuxième partie de la trame est composée des coordonnées matricielle des données que l'on veut, chaque coordonné est stocké sur un unsigned char pour prendre le moins de place possible, la coordonnées maximum vaut 15 et est donc inférieur à la valeur maximum pouvant être stockée sur un unsigned char (255).
 3. Le server répond avec un trame dont les 4 premiers octets contiennent la taille des données dans la suite de la trame.
 4. La deuxième partie de la trame contient les données dans l'ordre des coordonnées envoyées et sous forme d'unsigned double.
 
-**Queue:**
-La queue permet de simuler l'arrivée toutes les 33 ms d'une nouvelle matrice.
-Celle-ci permet la communication entre le while générant une nouvelle matrice toutes les 33 ms et le programme principal.
-On l'initialise comme ne pouvant contenir qu'un seul élément à la fois. En effet, soit la données est lue soit elle va être réecrite.
+## Performance:
 
-**Intégration au programme existant: **
-
-- Pour matcher avec notre programme, il faut donner la matrice à l'entrée de la création de la queue, ce qui permet à la queue d'avoir le bon type de donnée qu'elle va stocker.
-- Récuperer le "queue out", contenant le queue refnum et le type des données échangé avec la queue , et le "error in". Cette connexion entre le VI du programme de récolte de données et notre programme se fait dans le VI "Server_LabView.vi" en remplaçant le vi "data array refres.vi" par le VI du programme.
-- Une fois les données générées il faut, effacer ce qu'il y a dans la queue (s'il y a qq chose) avec le module dequeue element avec le time out à 0. Puis mettre les données générées dans la queue avec le module Enqueue element (sans time out).
-
-**Performance:**
 Pour une matrice de 15 par 10, on peut monter à 100Hz. On envoit donc 150 double (8 octets) en 0.01s donc la transmission est de 150 * 8 * 100 = 120ko/s.
--- client.py
+++ client.py
@@ -1,91 +1,17 @@
-"""
-This file is part of biosiglive. It allows connecting to a biosiglive server and to receive data from it.
-"""
-
 import socket
 import struct
 import time
-from typing import Union
 import numpy as np
 
 Buff_size = 32767
 
-
-class Message:
-    def __init__(self,
-                 command: list = (),
-                 read_frequency: float = 100,
-                 nb_frame_to_get: int = 1,
-                 get_names: bool = None,
-                 mvc_list: list = None,
-                 kalman: bool = None,
-                 get_raw_data: bool = False,
-                 **kwargs):
-        """
-        Message class
-        """
-
-        self.command = command
-        self.emg_windows = 2000
-        self.get_names = False
-        self.nb_frames_to_get = 1
-        self.get_names = get_names
-        self.mvc_list = mvc_list
-        self.kalman = kalman
-        self.read_frequency = read_frequency
-        self.nb_frames_to_get = nb_frame_to_get
-        self.raw_data = get_raw_data
-        for key in kwargs.keys():
-            self.__setattr__(key, kwargs[key])
-
-    def update_command(self, name: Union[str, list], value: Union[bool, int, float, list, str]):
-        """
-        Update the command.
-
-        Parameters
-        ----------
-        name: str
-            Name of the command to update.
-        value: bool, int, float, list, str
-            Value of the command to update.
-        """
-        names = [name] if not isinstance(name, list) else value
-        values = [value] if not isinstance(value, list) else value
-        values = [values] if name == "command" else values
-
-        for i, name in enumerate(names):
-            self.__setattr__(name, values[i])
-
-    def get_command(self):
-        """
-        Get the command.
-
-        Returns
-        -------
-        message: Message.dic
-            Message containing the command.
-        """
-        return self.command
-
-    def add_command(self, name: str, value: Union[bool, int, float, list, str]):
-        """
-        Add a command.
-
-        Parameters
-        ----------
-        name: str
-            Name of the command to add.
-        value: bool, int, float, list, str
-            Value of the command to add.
-        """
-        new_value = None
-        old_value = self.get_command()[name]
-        if isinstance(old_value, list):
-            old_value.append(value)
-            new_value = old_value
-        elif isinstance(old_value, (bool, int, float, str)):
-            new_value = value
-        return self.update_command(name, new_value)
+# Choose the behaviour of the program :
+#  - "SLOW" : send a data request for 2 values each 2 seconds. Use for debug and verify is the data received correspond
+#             to the data generated.
+#  - "NORMAL" : send a data request for all 150 values each 33ms. Correspond to the normal use of the program.
+#  - "FAST" : send a data request for all 150 values as fast possible. Use for test of speed.
+# The behaviour is of most use when the data generation in LabView is done on the same oder of magnitude than data # request.
+MODE_OF_EXECUTION = "NORMAL"
 
 
 class Client:
@@ -110,21 +36,16 @@ class Client:
         self.server_address = server_ip
         self.port = port
         self.client = self.client_sock(self.type)
-
-    def _connect(self):
-        self.client.connect((self.server_address, self.port))
-
-        self.time_total = 0
+        self.time_total_request_reception = 0
 
     @staticmethod
-    def client_sock(type: str,):
+    def client_sock(type: str, ):
         """
         Create a client main.
         Parameters
         ----------
         type: str
             Type of the main.
-
         Returns
         -------
         client: socket.socket
@@ -137,9 +58,14 @@ class Client:
         else:
             raise RuntimeError(f"Invalid type of connexion ({type}). Type must be 'TCP' or 'UDP'.")
 
+    def connect(self):
+        self.client.connect((self.server_address, self.port))
+
+        self.time_total_request_reception = 0
+
     def _recv_all(self, buff_size: int = Buff_size):
         """
-        Receive all data from the server.
+        Receive all data from the server and process it to return a tuple.
         Parameters
         ----------
         buff_size: int
@@ -147,8 +73,8 @@ class Client:
 
         Returns
         -------
-        data: list
-            List of data received.
+        data: tuple
+            Tuple of data received.
         """
 
         msg_len = self.client.recv(4)
@@ -157,17 +83,43 @@ class Client:
         msg_len = struct.unpack('!i', msg_len)[0]
 
         data = b''
-        l = 0
-        while l < msg_len:
+        length_read = 0
+        while length_read < msg_len:
             chunk = self.client.recv(buff_size)
-            l += len(chunk)
+            length_read += len(chunk)
             data += chunk
 
         # Unpack double in network standard (Big endian)
         frmt = '!' + str(int(msg_len)) + 'd'
         data = struct.unpack(frmt, data)
-        self.time_total = time.time() - self.time_total
+        self.time_total_request_reception = time.time() - self.time_total_request_reception
         return data
+
+    def _send_message(self, message: list):
+        """
+        Convert message to bytes and send it.
+        Parameters
+        ----------
+        message: list
+            List that will be sent
+        """
+
+        # Convert message in bytes type
+        b_message = b''
+        for i in range(np.shape(message)[0]):
+            for j in range(np.shape(message)[1]):
+                # we chose unsigned char 8 (range from 0 to 511) so each x or y coordinates can be store on 1 byte
+                b_message += struct.pack('!B', message[i][j])
+
+        # Convert int in bytes type
+        # The length is stored on an int by convention.
+        b_size = struct.pack('!i', np.shape(message)[0] * np.shape(message)[1] * 1)
+
+        # Send size of the message
+        self.client.sendall(b_size)
+
+        # Send message
+        self.client.sendall(b_message)
 
     def get_data(self, message, buff: int = Buff_size):
         """
@@ -184,59 +136,58 @@ class Client:
         data: dict
             Data from server.
         """
-        self.time_total = time.time()
-        if isinstance(message, Message):
-            message = message.__dict__
-
-        # Send text in byte form
-        b_message = b''
-        for i in range(np.shape(message)[0]):
-            for j in range(np.shape(message)[1]):
-                b_message += struct.pack('!B', message[i][j])
-
-        # Send int in byte form
-        # TODO: Change in order to correspond to an array
-        # we chose unsigned int 8 (range from 0 to 511) so each x or y coordinates can be store on 1 byte
-        b_size = struct.pack('!i', np.shape(message)[0] * np.shape(message)[1] * 1)
-
-        # Send size of the message
-        self.client.sendall(b_size)
-
-        # Send message
-        self.client.sendall(b_message)
+        self.time_total_request_reception = time.time()
+        self._send_message(message)
         return self._recv_all(buff)
 
 
-array_dictionary = {"FGx": [0, 0], "FGy": [0, 1], "FGz": [0, 2], "MGx": [2,0]}
+# Correspondence table between name of the value and their coordinates
+array_dictionary = {"FGx": [0, 0], "FGy": [0, 1], "FGz": [0, 2], "FDx": [0, 3], "FDy": [0, 4], "FDz": [0, 5],
+                    "MGx": [2, 0], "MGy": [2, 1], "MGz": [2, 2], "MDx": [2, 3], "MDy": [2, 4], "MDz": [2, 5],
+                    "TG": [4, 0], "TD": [4, 1],
+                    "AG": [6, 0], "AD": [6, 1]}
 
+# Main execution
 if __name__ == '__main__':
-    # Set program variables
-    type_of_data = ["force_gauche"]
-
+    # index give the number of request made, is reset to 1 at 100
+    index = 1
+    # time_read_tcp_ip is used to measure the average time of the time needed to send a command and received the data
+    time_read_tcp_ip = 0
 
     # Run streaming data
     host_ip = 'localhost'
     host_port = 6000
+
+    # Set the command containing the data request that will be sent to LabView
     command = []
-    # for i in range(15):
-    #     for j in range(10):
-    #         command.append([i, j])
-    command = [array_dictionary["FGx"], array_dictionary["MGx"]]
+    if MODE_OF_EXECUTION == "SLOW":
+        command = [array_dictionary["FGx"], array_dictionary["MGx"]]
+
+    elif (MODE_OF_EXECUTION == "NORMAL") | (MODE_OF_EXECUTION == "FAST"):
+        for i in range(15):
+            for j in range(10):
+                command.append([i, j])
+
     client = Client(host_ip, host_port, "TCP")
-    client._connect()
-    i = 1
+    client.connect()
+
     time_past = time.time()
-    time_read_tcp_ip = 0
 
     while True:
-        if i == 100:
-            print(round(time.time() - time_past, 6), time_read_tcp_ip/100)
+        if index == 100:
+            print(round(time.time() - time_past, 6), time_read_tcp_ip / 100)
             time_past = time.time()
-            i = 0
+            index = 0
             time_read_tcp_ip = 0
 
         data = client.get_data(command)
-        print(data)
-        i += 1
-        time_read_tcp_ip += client.time_total
-        time.sleep(2)
+
+        index += 1
+        time_read_tcp_ip += client.time_total_request_reception
+
+        if MODE_OF_EXECUTION == "SLOW":
+            print(data)
+            time.sleep(2 - client.time_total_request_reception)
+        elif MODE_OF_EXECUTION == "NORMAL":
+            if 0.033 > client.time_total_request_reception:
+                time.sleep(0.033 - client.time_total_request_reception)
